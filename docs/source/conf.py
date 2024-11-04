52898464e46ae4e11fa15db5450ae6cf4b1a4065
Benny Thadikaran <tbenny05@yahoo.com>
2024-11-04 11:46:04 +0530

    - Bump to version 1.0.1 - Updated docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, src.as_posix())
 project = "fast_csv_loader"
 copyright = "2024, Benny Thadikaran"
 author = "Benny Thadikaran"
-release = "1.0.0-2"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
